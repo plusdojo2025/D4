90927ba1f8fa701902d62943298106eea026b287
plusdojo2025-Hirose <hirose-hajime-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 13:28:43 +0900

    FrinedとIconのdtoを変更

--- source/src/main/java/dto/Friend.java
+++ source/src/main/java/dto/Friend.java
@@ -1,5 +1,39 @@
 package dto;
 
-public class Friend {
+import java.io.Serializable;
 
+public class Friend implements Serializable{
+	private String myId;
+	private String friendId;
+	private int state;
+	
+	public String getMyId() {
+		return myId;
+	}
+	public void setMyId(String myId) {
+		this.myId = myId;
+	}
+	public String getFriendId() {
+		return friendId;
+	}
+	public void setFriendId(String friendId) {
+		this.friendId = friendId;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+	public Friend(String myId, String friendId, int state){
+		this.myId = myId;
+		this.friendId = friendId;
+		this.state = state;
+	}
+	public Friend() {
+		this.myId = "";
+		this.friendId = "";
+		this.state = 0;
+	}
 }
--- source/src/main/java/dto/Icon.java
+++ source/src/main/java/dto/Icon.java
@@ -1,5 +1,40 @@
 package dto;
 
-public class Icon {
+import java.io.Serializable;
 
+public class Icon implements Serializable{
+	private int days;
+	private int id;
+	private String pass;
+	
+	public int getDays() {
+		return days;
+	}
+	public void setDays(int days) {
+		this.days = days;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	public Icon(int id, int days, String pass) {
+		this.id = id;
+		this.days = days;
+		this.pass = pass;
+	}
+	
+	public Icon() {
+		this.id = 0;
+		this.days = 0;
+		this.pass="";
+	}
 }
