efb48ccb9ac99398bb0d7cefef9b41b611c951cd
hashimotorinaD <hashimoto-rina-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 14:13:02 +0900

    テーマDTO更新

--- source/src/main/java/dto/Theme.java
+++ source/src/main/java/dto/Theme.java
@@ -1,5 +1,39 @@
 package dto;
 
-public class Theme {
+import java.io.Serializable;
+
+
+public class Theme implements Serializable {
+
+    private int id;                  // テーマID
+    private String name;             // テーマ名
+
+
+    public Theme() {
+    	this.id = 0;
+    	this.name = "";
+    	}
+
+    public Theme(int id, String name) {
+        this.id = id;
+        this.name = name;        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
+
